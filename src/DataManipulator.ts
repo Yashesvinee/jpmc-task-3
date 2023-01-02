8fa575dab739d48394d361f565b036bfb2715850
Yashesvinee V <yashesvineev@gmail.com>
2023-01-02 21:00:59 +0530

    Modified graph and datamanipulator

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,33 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+      price_abc:number,
+      price_def:number,
+      ratio:number,
+      upper_bound:number,
+      lower_bound:number,
+      trigger_alert:number|undefined,
+      timestamp:Date
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
+  static generateRow(serverResponds: ServerRespond[]) : Row {
+  const priceabc=(serverResponds[0].top_ask.price+serverResponds[0].top_bid.price)/2;
+  const pricedef=(serverResponds[1].top_ask.price+serverResponds[1].top_bid.price)/2;
+  const ratio=priceabc/pricedef;
+  const upperb=1+.10;
+  const lowerb=1-.10;
+
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+      price_abc:priceabc,
+      price_def:pricedef,
+      ratio,
+      timestamp:serverResponds[0].timestamp>serverResponds[1].timestamp?serverResponds[0].timestamp:serverResponds[1].timestamp,
+      upper_bound:upperb,
+      lower_bound:lowerb,
+      trigger_alert:(ratio>upperb || ratio<lowerb)? ratio:undefined,
       };
-    })
+
   }
 }
